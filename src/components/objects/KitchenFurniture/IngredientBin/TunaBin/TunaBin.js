60d0a439d2cbaa03c5e9f8b99a1353bd4c66873b
emluo555 <emilyluo555@gmail.com>
2025-12-11 20:33:11 -0500

    tunabin and ricebot

---
+++ src/components/objects/KitchenFurniture/IngredientBin/TunaBin/TunaBin.js
@@ -0,0 +1,19 @@
+import { sharedLoader } from "../../../loader";
+import MODEL from "./tunabin.glb";
+import IngredientBin from "../IngredientBin";
+
+class TunaBin extends IngredientBin {
+  constructor(parent, row = 0, col = 0) {
+    super(parent, row, col, "salmon");
+
+    // load specific model
+    this.name = "tunabin";
+    sharedLoader.load(MODEL, (gltf) => {
+      this.add(gltf.scene);
+      this.model = gltf.scene;
+      this.model.scale.set(0.5, 0.5, 0.5);
+    });
+  }
+}
+
+export default TunaBin;
