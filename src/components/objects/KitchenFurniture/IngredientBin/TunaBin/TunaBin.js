04e90e472d3be9145c2bbd6cdd8a30654b141f3d
Maxwell Lin <ml7013@princeton.edu>
2025-12-13 10:07:54 -0500

    make ingredient bins face correct direction relative to camera

--- src/components/objects/KitchenFurniture/IngredientBin/TunaBin/TunaBin.js
+++ src/components/objects/KitchenFurniture/IngredientBin/TunaBin/TunaBin.js
@@ -12,6 +12,7 @@ class TunaBin extends IngredientBin {
       this.add(gltf.scene);
       this.model = gltf.scene;
       this.model.scale.set(0.5, 0.5, 0.5);
+      this.model.rotation.y = Math.PI;
     });
   }
 }
